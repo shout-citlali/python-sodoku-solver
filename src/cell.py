e7e93bba91325ad6fed4feedb9c5c1d2119f704d
Stephanie Miller <shout.citlali@gmail.com>
2022-04-14 21:25:29 -0600

    zero based cell ids

--- src/cell.py
+++ src/cell.py
@@ -1,6 +1,6 @@
 class Cell:
     def __init__(self, row_id, column_id):
-        self.id = (row_id - 1) * 9 + column_id
+        self.id = row_id * 9 + column_id
         self.row_id = row_id
         self.column_id = column_id
         self.north = None
--- src/gameboard.py
+++ src/gameboard.py
@@ -9,11 +9,11 @@ class Gameboard:
         self.screen.title("Sudoku Solver")
         self.screen.tracer(0)
         self.cells = []
-        for row in range(1, 10):
-            for col in range(1, 10):
+        for row in range(0, 9):
+            for col in range(0, 9):
                 self.cells.append(Cell(row, col))
         for cell in self.cells:
-            cell.east = self.cells[cell.id - 9 if cell.id % 9 == 0 else cell.id]
+            cell.east = self.cells[cell.id - 8 if (cell.id + 1) % 9 == 0 else cell.id + 1]
             # cell.west = self.cells[cell.id + 9 if cell.id % 9 == 0 else cell.id]  # determine what cell west should be
         self.active_cell = self.cells[0]
 
@@ -34,14 +34,16 @@ class Gameboard:
             self.graphics.forward(900)
             self.graphics.setheading(0)
         self.graphics.penup()
-        self.graphics.goto(110 + 100 * (self.active_cell.column_id - 1), 110 + 100 * (self.active_cell.row_id - 1))
+        self.graphics.goto(110 + 100 * self.active_cell.column_id, 110 + 100 * self.active_cell.row_id)
         self.graphics.pendown()
-        self.graphics.forward(80)
+        for i in range(4):
+            self.graphics.forward(80)
+            self.graphics.left(90)
         self.graphics.penup()
         for cell in self.cells:
-            self.graphics.goto(cell.column_id * 100 + 50, cell.row_id * 100 + 65,)
+            self.graphics.goto(cell.column_id * 100 + 150, cell.row_id * 100 + 165,)
             self.graphics.write(cell.id, move=False, align="center", font=("Arial", 12, "normal"))
-            self.graphics.goto(cell.column_id * 100 + 80, cell.row_id * 100 + 65)
+            self.graphics.goto(cell.column_id * 100 + 180, cell.row_id * 100 + 165)
             self.graphics.write(cell.east.id, move=False, align="center", font=("Arial", 8, "normal"))
             self.graphics.goto(cell.column_id * 100 - 80, cell.row_id * 100 - 65)
             # self.graphics.write(cell.west.id, move=False, align="center", font=("Arial", 8, "normal"))# print west neighbor on left side of each square
